312426e1abec0cba81be2d631b893fe3e380c040
Daniella Bojado <daniella.bojado@gmail.com>
2020-10-06 14:51:32 -0500

    add Preparefile

---
+++ Prepare.py
@@ -0,0 +1,18 @@
+# Import libraries
+import pandas as pd
+from Acquire import get_mvp_home_data
+
+# Function to prepare the data
+def prep_zillow_df(df):
+    # Dropped null values
+    df = df.dropna()
+    return df
+
+
+# Function to split the data into train, validate, and test
+def train_test_validate(df):   
+# split into train, validate, and test sets
+    train_and_validate, test = train_test_split(df, test_size = .10, random_state=123)
+    train, validate = train_test_split(train_and_validate, test_size = .20, random_state=123)
+
+    return train, validate, test
